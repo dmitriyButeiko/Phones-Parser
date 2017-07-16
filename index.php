98d46af1dbb6225ba0609f17447eaeb8ad2aee2b
Dmitriy <dmitriy.buteiko@mail.ru>
2017-07-16 05:12:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php 
-	// Turn on errors and increase time execution time
-	set_time_limit(99999999);
-	ini_set('display_errors', 1);
+<?php
+    // Turn on errors and increase time execution time
+    set_time_limit(100000);
+    ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-	require_once "includes/PhonesParser.php";		
-	
-	$phonesParser = new PhonesParser();
-	$phonesParser->parseEmailData();
+    // Execute phones parsing
+    require_once "includes/PhonesParser.php";			
+    $phonesParser = new PhonesParser();
+    $phonesParser->parseEmailData();
 ?>
